d2fccd83d345aa436b581939c353d0bbd813e477
thingung <graeg@Elijahs-MBP-41.lan>
2025-03-28 11:08:07 -0700

    Added tracking of all links in a channel and the ability to call for any user

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-
-# Regular expression to check for URLs in a message
-url_pattern = re.compile(r'https?://[^\s]+')
+# Define the channel ID where you want to count the links
+TARGET_CHANNEL_ID = 1347815817080999969
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    # Get the channel by ID
+    channel = client.get_channel(TARGET_CHANNEL_ID)
+    if channel:
+        await user_reaction_util.count_links_in_channel(channel)
 
 @client.event
 async def on_message(message):
@@ -26,10 +29,13 @@ async def on_message(message):
     converted_url = convert_link(message)
     if converted_url:
         await message.channel.send(converted_url)
-    if url_pattern.search(message.content):
+    if user_reaction_util.url_pattern.search(message.content):
         await user_reaction_util.url_posted(message)
     if message.content.startswith(f'$links'):
         await message.channel.send(await user_reaction_util.get_user_link_count(message.author.id))
+    if message.content.startswith(f'$link'):
+        command_text = message.content[len('$link'):].strip()
+        await message.channel.send( await user_reaction_util.get_other_user_link_count(int(command_text)))
 
 
 
--- user_reaction_util.py
+++ user_reaction_util.py
@@ -1,8 +1,12 @@
 import discord
+import re
 from collections import defaultdict
 
 # Dictionary to store how many times a user has posted a link
 user_link_count = defaultdict(int)
+
+# Regular expression to check for URLs in a message
+url_pattern = re.compile(r'https?://[^\s]+')
 
 # Increments user link count
 async def url_posted(message):
@@ -12,3 +16,11 @@ async def url_posted(message):
 async def get_user_link_count(user_id):
     return user_link_count.get(user_id, 0)
 
+async def get_other_user_link_count(user_id):
+    return user_link_count.get(user_id, 0)
+
+# Fetch all messages and counting links for every user in a specific channel
+async def count_links_in_channel(channel):
+    async for message in channel.history(limit=100):  # Adjust limit as needed
+        if url_pattern.search(message.content):
+            user_link_count[message.author.id] += 1
